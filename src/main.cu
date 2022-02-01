9461066fc36aa85055f759efa6ab4304f49125f7
Mondus <p.richmond@sheffield.ac.uk>
2022-02-01 12:54:27 +0000

    uncomment experiment

--- src/main.cu
+++ src/main.cu
@@ -312,8 +312,8 @@ int main(int argc, const char** argv) {
     experiments.push_back(visualisationExperiment);
 #else
     // Performacne sclaing experiment to recoprd performance with increase in model size
-    //Experiment performance_scaling("performance_scaling", 256, 4096, 256, std::vector<float>({PROBABILITY_OF_OCCUPATION}), BENCHMARK_REPETITIONS, BENCHMARK_STEPS, false);
-    //experiments.push_back(performance_scaling);
+    Experiment performance_scaling("performance_scaling", 256, 4096, 256, std::vector<float>({PROBABILITY_OF_OCCUPATION}), BENCHMARK_REPETITIONS, BENCHMARK_STEPS, false);
+    experiments.push_back(performance_scaling);
 
     Experiment resolution_steps("resolution_steps", 512, 512, 512, std::vector<float>({ 0.02f,0.04f,0.08f,0.16f,0.32f,0.64f }), BENCHMARK_REPETITIONS, 3, true);
     experiments.push_back(resolution_steps);
